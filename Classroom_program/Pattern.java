0cac9a3d511404bebfb272ab0a8e348398ba624a
Ram Vishnu Bhumbar <rambhaiya0214@gmail.com>
2025-08-13 13:56:31 +0530

    Add files via upload

---
+++ Classroom_program/Pattern.java
@@ -0,0 +1,16 @@
+package Classroom_program;
+import java.util.Scanner;
+public class Pattern{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("Enter number of rows: ");
+		        int rows = scanner.nextInt();
+		        for (int i = 1; i <= rows; i++) {
+		            for (int j = 1; j <= i; j++) {
+		                System.out.print("*");
+		            }
+		            System.out.println();
+		        }
+		    }
+	}
